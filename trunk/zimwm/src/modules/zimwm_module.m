ab635534952ffa6af4d1aa99216e504a1d22ae23
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-04-18 16:19:45 +0000

    Module fixies

--- trunk/zimwm/src/modules/zimwm_module.m
+++ trunk/zimwm/src/modules/zimwm_module.m
@@ -5,8 +5,16 @@ ZWindow *zimwm_module_create_window(void)
 {
 	ZWindow *w = [ZWindow alloc];
 	XMapRequestEvent ev;
+	XSizeHints shints;
 
 	[w init:NULL:0:0:100:50];
+
+	shints.min_width = 20;
+	shints.min_height = 20;
+	shints.width_inc = 3;
+	shints.height_inc = 3;
+
+	XSetWMNormalHints(zdpy,(Window)w->window,&shints);
 
 	[w show];
 	ev.window = w->window;
