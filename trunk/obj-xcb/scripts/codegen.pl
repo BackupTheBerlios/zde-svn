476f9e0ba65ef63ddacdd202a4b2f591a44af0af
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-06-12 18:45:08 +0000

    start on the script

--- trunk/obj-xcb/scripts/codegen.pl
+++ trunk/obj-xcb/scripts/codegen.pl
@@ -3,5 +3,33 @@ use strict;
 
 use XML::Twig;
 
+#figure out what we are creating and what the input should be
+my $outfile = $ARGV[0];
+my $infile;
+
+if($outfile eq "objxcb_types.h") {
+	$infile = $ARGV[1] . "/xcb_types.xml";
+
+	#create twig
+	my $twig = XML::Twig->new(twig_handlers=> {
+		xcb => \&xcb_handle,
+		xidtype => \&xid_handle
+		}
+		);
+
+	$twig->parsefile("$infile");
+}
+
+
+sub xcb_handle()
+{
+
+}
+
+sub xid_handle()
+{
+
+}
+
 
 0;
