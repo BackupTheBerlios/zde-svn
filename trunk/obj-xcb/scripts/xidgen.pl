9f3077f886b17e943fb14648ad95b799ac51af0f
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-06-13 20:14:32 +0000

    generate the constructor and deconstructor declarations

--- trunk/obj-xcb/scripts/xidgen.pl
+++ trunk/obj-xcb/scripts/xidgen.pl
@@ -7,7 +7,7 @@ use strict;
 use XML::Twig;
 
 sub start_class_header($);
-sub start_class_source($);
+sub start_class_source($$);
 sub request_handle();
 
 #create the output filenames
@@ -21,6 +21,9 @@ my $outfile = "xcb_" . $xid;
 my $outheaderfile = $outfile . ".h";
 my $outsourcefile = $outfile . ".m";
 
+start_class_header($outheaderfile);
+start_class_source($outsourcefile,$outheaderfile);
+
 #create twig
 my $twig = XML::Twig->new(twig_handlers=> {
 		request => \&request_handle
@@ -28,9 +31,6 @@ my $twig = XML::Twig->new(twig_handlers=> {
 
 $twig->parsefile($ARGV[1]);
 $twig->purge;
-
-start_class_header($outheaderfile);
-start_class_source($outsourcefile);
 
 sub request_handle()
 { my($twig, $section) = @_;
@@ -60,8 +60,7 @@ sub start_class_header($)
 
 	open($headerfh,">",$_[0]) or die("Couldn't open header file.");
 	#copyright
-	print $headerfh '
-/*
+	print $headerfh '/*
    Copyright (c) 2005, 2006 Thomas Coppi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -70,14 +69,23 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+ This code has been automatically generated, modify at your own risk.
+
  */
 ';
 
 	#interface declaration (needs to take into accout WINDOW and PIXMAP, which inherit from DRAWABLE
-	print $headerfh "\@interface ObjXCB$capxid : Object \n{ ";
+	print $headerfh "\@interface ObjXCB$capxid : Object \n{\n ";
+
+	#all classes have init and free
+	print $headerfh '/** Creates a new XID and initializes the object. */' . "\n";
+	print $headerfh '- (id)init;' . "\n";
+	print $headerfh '/** Takes in an XID and initializes the object. */' . "\n";
+	print $headerfh '- (id)init:(' . "XCB".$ARGV[0] . ')xid;' . "\n";
+	print $headerfh '- free;' ."\n";
 }
 
-sub start_class_source($)
+sub start_class_source($$)
 {
 
 }
