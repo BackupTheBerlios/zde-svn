7696e54ddf9b1652c5c9ea6aa09a405c172875c2
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-06-13 16:46:11 +0000

    Cleanups and clarifications

--- trunk/obj-xcb/src/obj-xcb.h
+++ trunk/obj-xcb/src/obj-xcb.h
@@ -24,5 +24,5 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "xcb_conn.h"
 
 /* XXX Currently deprecated. */
-#include "xcb_win.h"
+//#include "xcb_window.h"
 
