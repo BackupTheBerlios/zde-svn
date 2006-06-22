d4f68b2d3bc28d29fccaee050ee6c1e9356223b8
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-06-22 21:15:30 +0000

    A bit more advanced test program

--- trunk/obj-xcb/tests/main.m
+++ trunk/obj-xcb/tests/main.m
@@ -6,7 +6,13 @@ int main(void)
 	ObjXCBWindow *w = [ObjXCBWindow alloc];
 	ObjXCBWindow *root = [ObjXCBWindow alloc];
 	ObjXCBGetGeometryReply *geomrep;
+	ObjXCBGcontext *gc = [ObjXCBGcontext alloc];
+
+	XCBGenericEvent *e;
 	XCBSCREEN *s;
+	XCBPOINT p[2];
+	CARD32 value[1];
+	CARD32 wvalue[2];
 
 	/* Create a connection to the server. */
 	[c init];
@@ -17,18 +23,44 @@ int main(void)
 
 	/* Initialize and create a window */
 	[w init:c];
-	[w CreateWindow:XCBCopyFromParent:root:0:0:100:200:1:XCBWindowClassInputOutput:s->root_visual:0:NULL];
+	wvalue[0] = [c get_black_pixel];
+	wvalue[1] = XCBEventMaskExposure;
+	[w CreateWindow:XCBCopyFromParent:root:0:0:100:200:1:XCBWindowClassInputOutput:s->root_visual:XCBCWEventMask | XCBGCForeground:wvalue];
 	[w MapWindow];
 
 	geomrep = [w GetGeometry];
+
+	/* Create a graphics context for the window */
+	[gc init:c];
+
+	value[0] = [c get_white_pixel];
+	[gc CreateGC:w:XCBGCForeground:value];
+
+	/* Setup the points for a line */
+	p[0].x = 10;
+	p[0].y = 10;
+	p[1].x = 80;
+	p[1].y = 100;
+
 
 	/* Flush all requests to the server */
 	[c flush];
 
 	printf("The window is positioned at x:%d:y:%d with width:%d and height:%d.\n",[geomrep get_x],[geomrep get_y],[geomrep get_width],[geomrep get_height]);
 
-	/* Wait for a second so we can actually see the window */
-	sleep(1);
+	while(e = [c next_event]) {
+		switch(e->response_type) {
+			case XCBExpose:
+				[w PolyLine:XCBCoordModeOrigin:gc:2:p];
+				[c flush];
+				break;
+			default:
+				printf("he\n");
+				break;
+		}
+
+		free(e);
+	}
 
 	[geomrep free];
 	[c free];
