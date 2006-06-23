ac44eb242b88af823fed4c95af698604105d75cd
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-06-23 19:10:32 +0000

    Interesting little test program that draws a bunch of black and white rectangles in random positions

--- trunk/obj-xcb/tests/main.m
+++ trunk/obj-xcb/tests/main.m
@@ -6,17 +6,21 @@ int main(void)
 	ObjXCBWindow *w = [ObjXCBWindow alloc];
 	ObjXCBWindow *root = [ObjXCBWindow alloc];
 	ObjXCBGetGeometryReply *geomrep;
-	ObjXCBGcontext *gc = [ObjXCBGcontext alloc];
+	ObjXCBGcontext *gc;
+	ObjXCBGcontext *bgc = [ObjXCBGcontext alloc];
+	ObjXCBGcontext *wgc = [ObjXCBGcontext alloc];
 
 	XCBGenericEvent *e;
 	XCBSCREEN *s;
-	XCBPOINT p[2];
+	XCBRECTANGLE rect[1];
 	CARD32 value[1];
 	CARD32 wvalue[2];
 
 	/* Create a connection to the server. */
 	[c init];
 	
+	srand(time(NULL));
+
 	/* Get the default screen and root window */
 	s = [c get_screen];
 	root = [c get_root_window];
@@ -25,46 +29,70 @@ int main(void)
 	[w init:c];
 	wvalue[0] = [c get_black_pixel];
 	wvalue[1] = XCBEventMaskExposure;
-	[w CreateWindow:XCBCopyFromParent:root:0:0:100:200:1:XCBWindowClassInputOutput:s->root_visual:XCBCWEventMask | XCBGCForeground:wvalue];
+	[w CreateWindow:XCBCopyFromParent:root:0:0:300:400:1:XCBWindowClassInputOutput:s->root_visual:XCBCWEventMask | XCBGCForeground:wvalue];
 	[w MapWindow];
 
 	geomrep = [w GetGeometry];
 
 	/* Create a graphics context for the window */
-	[gc init:c];
+	[wgc init:c];
+	[bgc init:c];
 
 	value[0] = [c get_white_pixel];
-	[gc CreateGC:w:XCBGCForeground:value];
+	[wgc CreateGC:w:XCBGCForeground:value];
+	value[0] = [c get_black_pixel];
+	[bgc CreateGC:w:XCBGCForeground:value];
 
-	/* Setup the points for a line */
-	p[0].x = 10;
-	p[0].y = 10;
-	p[1].x = 80;
-	p[1].y = 100;
+
+	rect[0].x = 0;
+	rect[0].y = 0;
+	rect[0].width = [geomrep get_width];
+	rect[0].height = [geomrep get_height];
+	[w PolyFillRectangle:bgc:1:rect];
+
 
 	/* Flush all requests to the server */
 	[c flush];
 
 	printf("The window is positioned at x:%d:y:%d with width:%d and height:%d.\n",[geomrep get_x],[geomrep get_y],[geomrep get_width],[geomrep get_height]);
 
-	while(e = [c next_event]) {
-		switch(e->response_type) {
-			case XCBExpose:
-				[w PolyLine:XCBCoordModeOrigin:gc:2:p];
-				[c flush];
-				break;
-			default:
-				printf("he\n");
-				break;
+	while(1) {
+		e = [c poll_next_event:NULL];
+		if(e) {
+			switch(e->response_type) {
+				case XCBExpose:
+					geomrep = [w GetGeometry];
+				//	[w PolyFillRectangle:gc:1:rect];
+					break;
+				default:
+					break;
+			}
+
+			free(e);
+			e = NULL;
+		}	
+		if(rand() % 2 == 1) {
+			gc = bgc;
+		}
+		else {
+			gc = wgc;
 		}
 
-		free(e);
+		rect[0].x = rand() % [geomrep get_width];
+		rect[0].y = rand() % [geomrep get_height];
+		rect[0].width = rand() % [geomrep get_width];
+		rect[0].height = rand() % [geomrep get_height];	
+
+		[w PolyFillRectangle:gc:1:rect];
+		[c flush];
 	}
 
 	[geomrep free];
 	[c free];
 	[root free];
 	[w free];
+	[bgc free];
+	[wgc free];
 
 	return 0;
 }
