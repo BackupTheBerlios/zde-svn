7853ce0c0a8cf8dd70110fd30c427e1a2539e236
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-08-08 18:35:55 +0000

    took me forever to figure it out, but doxygen uses c++ declaration format for objective c methods. Stupid, stupid, stupid!!! wasted hours on that.

--- trunk/obj-xcb/src/documentation.h
+++ trunk/obj-xcb/src/documentation.h
@@ -1,4 +1,20 @@
+/* XXX What is really fucking stupid is that Doxygen only takes function decls in C++ format for Objective C.  Isn't that retarded? */
+
+/** \class ObjXCBDrawable
+ * \brief Represents an object that can be drawn to.
+ *
+ */
+
+/** \fn ObjXCBGetGeometryReply *ObjXCBDrawable::GetGeometry
+ * \brief Testing, 123
+ */
+
 /** \class ObjXCBPixmap
- * \brief A Pixmap
+ * \brief An object that represents drawable pixel data.
+ *
+ */
+
+/** \class ObjXCBWindow
+ *  \brief An object that represents visible drawable pixel data.
  *
  */
