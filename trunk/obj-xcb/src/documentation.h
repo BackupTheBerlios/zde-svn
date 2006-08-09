4e9c05dbd4e8b991852920c8e0da77d3c99d4134
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-08-09 16:21:25 +0000

    document the main reply module

--- trunk/obj-xcb/src/documentation.h
+++ trunk/obj-xcb/src/documentation.h
@@ -40,3 +40,21 @@
  * \brief A context for drawing onto ObjXCBDrawables.
  */
 
+
+/* Sections, these explain parts of Obj-XCB that don't belong anywhere else. */
+
+/**
+ * \addtogroup Replies
+ * @{
+ * 
+ * Reply object are returned by Obj-XCB methods that need to return multiple values.  The interesting thing about XCB is that replies are not actually
+ * retrieved when you call a function that returns, you are instead given an ID for the reply.  You then must call a special funtion that forces the reply,
+ * and gives you a structure containing values.  This allows you to queue up a string of functions that all return values, but only initiate the actual
+ * request when you need the values.  This allows you to strategically place queueing the request and actually calling the request in your code, in order to
+ * hide latency.  Obj-XCB takes advantage of this feature also, through the reply objects.  Whenever you call a request that in XCB would normally give you an
+ * ID for the request value, you are given the actual object that you use to access the values.  But, when you actually use the methods on the object to get at 
+ * the data, that is then when the reply is forced, not before.  Therefore, you can have the same power that is available in plain XCB without keeping track of 
+ * hundreds of different variables, instead it is all neatly encapsulated into a black box object that does the work for you.
+ *
+ */
+
