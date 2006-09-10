1cdd34902c72822b3caeb1f55aec59066e39e15c
thisnukes4u <thisnukes4u@826e13a0-b70b-0410-b3a2-a47d6dba90e2>
2006-09-10 19:51:09 +0000

    zpager is/will be a pager written with the zwl libraries and cairo, in order to make a icccm/netwm compatiable with lots of cool stuff

---
+++ trunk/zpager/src/main.m
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#include <cairo.h>
+#include <cairo-xcb.h>
+#include <cairo-xcb-xrender.h>
+
+#include <X11/XCB/xcb.h>
+#include <X11/XCB/xcb_renderutil.h>
+
+int main(void)
+{
+
+	return 0;
+}
